905b2617ae645cb9eeef01c5b0edbea6c3c979c9
carloslima27 <carloslimaplays2017@gmail.com>
2020-07-29 11:52:18 -0300

    Classes peca e Tabuleiro

--- src/Aplicacao/programaXadrez.java
+++ src/Aplicacao/programaXadrez.java
@@ -1,9 +1,12 @@
 package Aplicacao;
 
+import JogoTabulero.Tabuleiro;
+
 public class programaXadrez {
 
 	public static void main(String[] args) {
-		
+			
+		Tabuleiro t = new Tabuleiro(8, 8);
 
 	}
 
